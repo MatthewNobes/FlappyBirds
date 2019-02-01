48e13a3fad3b4eb93dd1bc4ba26a29749a01058c
MatthewNobes <mattnobes.personal@protonmail.com>
2019-02-01 19:37:25 +0000

    flappy bird final
    never going to touch this mess again

---
+++ FlappyBirds/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrOne = New System.Windows.Forms.Timer(Me.components)
+        Me.picBird = New System.Windows.Forms.PictureBox()
+        Me.picTube3 = New System.Windows.Forms.PictureBox()
+        Me.tmrTwo = New System.Windows.Forms.Timer(Me.components)
+        Me.picTube4 = New System.Windows.Forms.PictureBox()
+        Me.picTube2 = New System.Windows.Forms.PictureBox()
+        Me.picTube1 = New System.Windows.Forms.PictureBox()
+        Me.picTube5 = New System.Windows.Forms.PictureBox()
+        Me.picTube0 = New System.Windows.Forms.PictureBox()
+        Me.picTube9 = New System.Windows.Forms.PictureBox()
+        Me.picTube8 = New System.Windows.Forms.PictureBox()
+        Me.picTube7 = New System.Windows.Forms.PictureBox()
+        Me.picTube6 = New System.Windows.Forms.PictureBox()
+        CType(Me.picBird, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTube6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tmrOne
+        '
+        '
+        'picBird
+        '
+        Me.picBird.BackColor = System.Drawing.Color.Transparent
+        Me.picBird.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picBird.Image = Global.FlappyBirds.My.Resources.Resources.bird_20seccyle
+        Me.picBird.Location = New System.Drawing.Point(12, 128)
+        Me.picBird.Name = "picBird"
+        Me.picBird.Size = New System.Drawing.Size(19, 16)
+        Me.picBird.TabIndex = 0
+        Me.picBird.TabStop = False
+        '
+        'picTube3
+        '
+        Me.picTube3.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.flappy_bird_pipe
+        Me.picTube3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube3.Location = New System.Drawing.Point(579, 242)
+        Me.picTube3.Name = "picTube3"
+        Me.picTube3.Size = New System.Drawing.Size(84, 193)
+        Me.picTube3.TabIndex = 1
+        Me.picTube3.TabStop = False
+        '
+        'tmrTwo
+        '
+        '
+        'picTube4
+        '
+        Me.picTube4.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.flappy_bird_pipe
+        Me.picTube4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube4.Location = New System.Drawing.Point(790, 350)
+        Me.picTube4.Name = "picTube4"
+        Me.picTube4.Size = New System.Drawing.Size(84, 85)
+        Me.picTube4.TabIndex = 2
+        Me.picTube4.TabStop = False
+        '
+        'picTube2
+        '
+        Me.picTube2.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.flappy_bird_pipe
+        Me.picTube2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube2.Location = New System.Drawing.Point(390, 299)
+        Me.picTube2.Name = "picTube2"
+        Me.picTube2.Size = New System.Drawing.Size(84, 136)
+        Me.picTube2.TabIndex = 3
+        Me.picTube2.TabStop = False
+        '
+        'picTube1
+        '
+        Me.picTube1.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.flappy_bird_pipe
+        Me.picTube1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube1.Location = New System.Drawing.Point(153, 183)
+        Me.picTube1.Name = "picTube1"
+        Me.picTube1.Size = New System.Drawing.Size(84, 252)
+        Me.picTube1.TabIndex = 4
+        Me.picTube1.TabStop = False
+        '
+        'picTube5
+        '
+        Me.picTube5.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.flappy_bird_pipe
+        Me.picTube5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube5.Location = New System.Drawing.Point(957, 163)
+        Me.picTube5.Name = "picTube5"
+        Me.picTube5.Size = New System.Drawing.Size(95, 272)
+        Me.picTube5.TabIndex = 5
+        Me.picTube5.TabStop = False
+        '
+        'picTube0
+        '
+        Me.picTube0.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.Flappy_pipe_upside_down
+        Me.picTube0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube0.Location = New System.Drawing.Point(153, -3)
+        Me.picTube0.Name = "picTube0"
+        Me.picTube0.Size = New System.Drawing.Size(84, 91)
+        Me.picTube0.TabIndex = 6
+        Me.picTube0.TabStop = False
+        '
+        'picTube9
+        '
+        Me.picTube9.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.Flappy_pipe_upside_down
+        Me.picTube9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube9.Location = New System.Drawing.Point(390, -3)
+        Me.picTube9.Name = "picTube9"
+        Me.picTube9.Size = New System.Drawing.Size(84, 181)
+        Me.picTube9.TabIndex = 7
+        Me.picTube9.TabStop = False
+        '
+        'picTube8
+        '
+        Me.picTube8.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.Flappy_pipe_upside_down
+        Me.picTube8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube8.Location = New System.Drawing.Point(579, -3)
+        Me.picTube8.Name = "picTube8"
+        Me.picTube8.Size = New System.Drawing.Size(84, 147)
+        Me.picTube8.TabIndex = 8
+        Me.picTube8.TabStop = False
+        '
+        'picTube7
+        '
+        Me.picTube7.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.Flappy_pipe_upside_down
+        Me.picTube7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube7.Location = New System.Drawing.Point(790, -3)
+        Me.picTube7.Name = "picTube7"
+        Me.picTube7.Size = New System.Drawing.Size(84, 244)
+        Me.picTube7.TabIndex = 9
+        Me.picTube7.TabStop = False
+        '
+        'picTube6
+        '
+        Me.picTube6.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.Flappy_pipe_upside_down
+        Me.picTube6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picTube6.Location = New System.Drawing.Point(957, -5)
+        Me.picTube6.Name = "picTube6"
+        Me.picTube6.Size = New System.Drawing.Size(95, 67)
+        Me.picTube6.TabIndex = 10
+        Me.picTube6.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.FlappyBirds.My.Resources.Resources.flappy_bird_backgroundv2
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1064, 519)
+        Me.Controls.Add(Me.picTube6)
+        Me.Controls.Add(Me.picTube7)
+        Me.Controls.Add(Me.picTube8)
+        Me.Controls.Add(Me.picTube9)
+        Me.Controls.Add(Me.picTube0)
+        Me.Controls.Add(Me.picTube5)
+        Me.Controls.Add(Me.picTube1)
+        Me.Controls.Add(Me.picTube2)
+        Me.Controls.Add(Me.picTube4)
+        Me.Controls.Add(Me.picTube3)
+        Me.Controls.Add(Me.picBird)
+        Me.Name = "Form1"
+        Me.Text = "wdasdw"
+        CType(Me.picBird, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTube6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents tmrOne As System.Windows.Forms.Timer
+    Friend WithEvents picBird As System.Windows.Forms.PictureBox
+    Friend WithEvents picTube3 As System.Windows.Forms.PictureBox
+    Friend WithEvents tmrTwo As System.Windows.Forms.Timer
+    Friend WithEvents picTube4 As System.Windows.Forms.PictureBox
+    Friend WithEvents picTube2 As System.Windows.Forms.PictureBox
+    Friend WithEvents picTube1 As System.Windows.Forms.PictureBox
+    Friend WithEvents picTube5 As System.Windows.Forms.PictureBox
+    Friend WithEvents picTube0 As System.Windows.Forms.PictureBox
+    Friend WithEvents picTube9 As System.Windows.Forms.PictureBox
+    Friend WithEvents picTube8 As System.Windows.Forms.PictureBox
+    Friend WithEvents picTube7 As System.Windows.Forms.PictureBox
+    Friend WithEvents picTube6 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ FlappyBirds/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Dim score As Byte = 0
+    Dim message As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        tmrOne.Enabled = True
+    End Sub
+
+    Private Sub tmrOne_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrOne.Tick
+
+        picBird.Left = picBird.Left + 2
+        picBird.Top = picBird.Top + 4
+    End Sub
+
+    Private Sub Form1_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp
+        Select Case e.KeyCode
+            Case Keys.Up
+                picBird.Top -= 10
+        End Select
+
+        If CollidesWithPipe(picBird) = True Then
+            tmrOne.Enabled = False
+            tmrTwo.Enabled = True
+
+            Select Case picBird.Left
+                Case Is < 200
+                    score = 0
+                    message = "yr crap!"
+                Case Is < 300
+                    score = 2
+                    message = "meh"
+                Case Else
+                    score = 4
+                    message = "well done!"
+            End Select
+            picBird.Image.RotateFlip(RotateFlipType.Rotate90FlipNone)
+            MessageBox.Show(message & vbCr & " you scored " & score & " points")
+        End If
+
+    End Sub
+    Private Function CollidesWithPipe(ByVal Pic As PictureBox) As Boolean
+        Dim ctl As Control
+        For Each ctl In Me.Controls
+            If ctl Is picTube0 Or ctl Is picTube1 Or ctl Is picTube2 Or ctl Is picTube3 Or ctl Is picTube4 Or ctl Is picTube5 Or ctl Is picTube6 Or ctl Is picTube7 Or ctl Is picTube8 Or ctl Is picTube9 Then
+                If ctl.Bounds.IntersectsWith(Pic.Bounds) Then
+                    Return True
+                End If
+            End If
+        Next
+        Return False
+    End Function
+
+    Private Sub tmrTwo_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrTwo.Tick
+        picBird.Top = picBird.Top + 2
+        If picBird.Top > 250 Then
+            picBird.Top = 250
+        End If
+    End Sub
+End Class
+
